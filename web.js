1552c56778510c0beffeb9fdc808adacae86115c
Tobias <tobias.messenbrink@gmail.com>
2013-07-07 21:56:35 +0000

    updated web.js for external file

--- web.js
+++ web.js
@@ -3,10 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
-var buffer = (fs.readFileSync(index.html);
-
-  response.send(buffer.toString('utf-8')
+    var buf = fs.readFileSync(index.html);
+    response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
